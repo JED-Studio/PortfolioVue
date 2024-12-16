b323c049d019d71c4aa7431cecdc599c19e16811
Ramazan <romagt2015@mail.ru>
2024-12-16 14:58:24 +0300

    beginning

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="portfolio">
+    <header class="header">
+      <div class="container max-w-7xl mx-auto pl-4 pr-4">
+        <div class="flex items-center justify-end gap-6 py-4"></div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<style scoped></style>
